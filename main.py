6e1e521535871a0cfbfbd13b842e5ec6956eb6b9
ldir92664 <ldir92664@163.com>
2022-03-11 18:08:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main(event, context):
         'https://api.dler.io/sub?target=clash&new_name=true&url=https%3A%2F%2Fgfwglass.tk%2Fclash%2Fproxies%3Fnc%3DCN%2CHK%2CTW%26speed%3D100%26type%3Dvmess%2Ctrojan%2Cssr%2Css&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini', 'clash-pro.yml')
 
     savePoint(
-        'https://api.dler.io/sub?target=clash&new_name=true&url=https%3A%2F%2Fednovas.design%2Fclash%2Fproxies%3Fnc%3DCN%2CHK%2CTW%26speed%3D100%26type%3Dvmess%2Ctrojan%2Cssr%2Css&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini', 'ednovas.design.yml')
+        'https://api.dler.io/sub?target=clash&url=https%3A%2F%2Fednovas.design%2Fclash%2Fproxies%3Fnc%3DCN%2CHK%2CTW%26speed%3D50%2C100%26type%3Dvmess%2Ctrojan%2Cssr%2Css&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini', 'ednovas.design.yml')
     
     savePoint(
         'https://raw.githubusercontent.com/lizisan/Free-Proxies/main/README.md', 'README.md')
